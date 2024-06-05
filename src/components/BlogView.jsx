094d2c3cb1939e3e7fdacb919f6f32ae588d2593
Siddhartha Mukherjee <siddharthamukherjee0709@gmail.com>
2024-06-05 13:20:15 +0530

    add html rendering of page

--- src/components/BlogView.jsx
+++ src/components/BlogView.jsx
@@ -2,30 +2,48 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
-import ReactMarkdown from 'react-markdown';
-import '../components/markdown.css';
-import 'github-markdown-css/github-markdown-dark.css';
+import hljs from 'highlight.js'; // Ensure you have highlight.js installed
+import Loader from './Loader';
+
 function BlogView() {
     const location = useLocation();
     const blog = location.state;
-
-    const [markdownContent, setMarkdownContent] = useState('');
-
-    const fetchBlogContent = async ()=>{
+    const [blogFile, setBlog] = useState('');
+    const [loading, setLoading] = useState(true);
+    const fetchDocument = async (markdownUrl) => {
         try {
-            const response  = await axios.get(blog.markdownUrl);
-            setMarkdownContent(response.data);
-            console.log('Markdown content:', response.data);
+            const document = await axios.get(markdownUrl);
+            setBlog(document.data);
+            setLoading(false);
         } catch (error) {
-            console.error('Error fetching markdown file:', error);
+            // console.error('THis part',error);
         }
+    };
 
-    }
+    const getBlogFile = async () => {
+        console.log(blog._id);
+        try {
+            const url = `https://lobster-app-lh22k.ondigitalocean.app/api/v1/blogs/getBlogById/?id=${blog._id}`;
+            // console.log('The url is ' + url);
+            const response = await axios.get(url);
+            // console.log('The Response - ', response.data.blogData);
+            const markdownUrl = response.data.blogData.markdownUrl;
+            await fetchDocument(markdownUrl);
+        } catch (error) {
+            // console.error(error);    
+        }
+    };
 
     useEffect(() => {
-        fetchBlogContent();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+        getBlogFile();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+        if (blogFile) {
+            hljs.highlightAll();
+        }
+    }, [blogFile]);
 
     if (!blog) {
         return <p className="text-white">No blog data available</p>;
@@ -33,28 +51,26 @@ function BlogView() {
 
     return (
         <div className="min-h-screen flex flex-col bg-gray-950 text-white">
-            <main className="flex-grow py-12 px-4 md:px-6">
-                <div className="container mx-auto">
-                    {/* <h1 className="text-3xl font-bold mb-8">{blog.title}</h1> */}
-                    {/* <h1 className="text-white text-xl mb-4">
+            {loading ? (<Loader />) : (
+                <main className="flex-grow py-12 px-4 md:px-6">
+                    <div className="container mx-auto">
+                        <h1 className="text-3xl font-bold mb-8">{blog.title}</h1>
+                        {/* <h1 className="text-white text-xl mb-4">
                         {blog.shortDescription}
                     </h1> */}
-                    <div className="mb-8">
-                        <img
-                            src={blog.thumbnailUrl}
-                            alt={blog.title}
-                            className="w-full h-auto rounded-lg"
-                        />
+                        <div className="mb-8">
+                            <img
+                                src={blog.thumbnailUrl}
+                                alt={blog.title}
+                                className="w-full h-auto rounded-lg ml-auto mr-auto"
+                            />
+                        </div>
+                        <div className="bg-gray-950 rounded-lg overflow-auto">
+                            <div dangerouslySetInnerHTML={{ __html: blogFile }} />
+                        </div>
                     </div>
-                    <div className="bg-gray-950 rounded-lg overflow-auto">
-                        <ReactMarkdown className="markdown-body p-4">
-                            {markdownContent}
-                        </ReactMarkdown>
-                    </div>
-
-                </div>
-            </main>
-
+                </main>
+            )}
         </div>
     );
 }
